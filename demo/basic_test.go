5aacb42197b34cf9767ffab72f854a9ed8cc8ee0
xavier268 <xavier268fedora31onHPEnvy>
2019-12-08 04:10:07 +0100

    playing with tests

--- demo/basic_test.go
+++ demo/basic_test.go
@@ -9,52 +9,57 @@ import (
 	. "gorgonia.org/gorgonia"
 )
 
-func TestBasic(t *testing.T) {
+// Same graph is used for all tests.
+func makeGraph() (g *ExprGraph, z *Node) {
+	g = NewGraph()
 
-	g := NewGraph()
-
-	var x, y, z *Node
+	var x, y *Node
 	var err error
 
 	// Define expression
 	x = NewScalar(g, Float64, WithName("x"))
 	y = NewScalar(g, Float64, WithName("y"))
-	if z, err = Add(x, y); err != nil {
+	z, err = Mul(x, y)
+
+	if err != nil {
 		log.Fatal(err)
 	}
-
-	// Crete a VM to run the program on
-	machine := NewTapeMachine(g)
-	defer machine.Close()
 
 	// Set initial values and run
 	Let(x, 2.)
 	Let(y, 2.5)
+
+	return g, z
+}
+
+// TestBasic computes no gradient.
+func TestBasic(t *testing.T) {
+	var err error
+
+	g, z := makeGraph()
+
+	// Create a VM to run the program on
+	machine := NewTapeMachine(g)
+	defer machine.Close()
+
 	if err = machine.RunAll(); err != nil {
 		log.Fatal(err)
 	}
 
 	fmt.Printf("\nThe z value is : %v\n", z.Value())
 
+	// Writing graph as dot file
+	ioutil.WriteFile("basic.dot", []byte(g.ToDot()), 0644)
 }
 
-// TestAutodiff showcases automatic differentiation
+// TestAutodiff showcases automatic differentiation, computes all possible gradients.
+// Lisp machine is required.
 func TestAutodiff(t *testing.T) {
-	g := NewGraph()
 
-	var x, y, z *Node
 	var err error
 
-	// define the expression
-	x = NewScalar(g, Float64, WithName("x"))
-	y = NewScalar(g, Float64, WithName("y"))
-	if z, err = Add(x, y); err != nil {
-		log.Fatal(err)
-	}
-
-	// set initial values then run
-	Let(x, 2.0)
-	Let(y, 2.5)
+	g, z := makeGraph()
+	x, y := g.ByName("x")[0], g.ByName("y")[0]
 
 	// by default, LispMachine performs forward mode and backwards mode execution
 	m := NewLispMachine(g)
@@ -72,27 +77,24 @@ func TestAutodiff(t *testing.T) {
 	if ygrad, err := y.Grad(); err == nil {
 		fmt.Printf("dz/dy: %v\n", ygrad)
 	}
+
+	// Writing graph as dot file
+	ioutil.WriteFile("basic_autodiff.dot", []byte(g.ToDot()), 0644)
 }
 
+// testSymbolicDiff computes the requested gradients.
+// tapeMachine is enough.
 func TestSymbolicDiff(t *testing.T) {
-	g := NewGraph()
-
-	var x, y, z *Node
 	var err error
 
-	// define the expression
-	x = NewScalar(g, Float64, WithName("x"))
-	y = NewScalar(g, Float64, WithName("y"))
-	z, err = Add(x, y)
-	if err != nil {
-		log.Fatal(err)
-	}
+	g, z := makeGraph()
+	x, y := g.ByName("x")[0], g.ByName("y")[0]
 
 	// symbolically differentiate z with regards to x and y
 	// this adds the gradient nodes to the graph g
 	// grads is an array of the gradient wrt z
-	var grads Nodes
-	grads, err = Grad(z, x, y)
+	// var grads Nodes
+	_, err = Grad(z, x, y)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -100,9 +102,7 @@ func TestSymbolicDiff(t *testing.T) {
 	// create a VM to run the program on
 	machine := NewTapeMachine(g)
 
-	// set initial values then run
-	Let(x, 2.0)
-	Let(y, 2.5)
+	// then run
 	if machine.RunAll() != nil {
 		log.Fatal(err)
 	}
@@ -121,9 +121,7 @@ func TestSymbolicDiff(t *testing.T) {
 	}
 	fmt.Printf("dz/dy: %v\n", ygrad)
 
-	// Display all the gradients taht were calculated ...
-	fmt.Printf("\nGrads : %v\n", grads)
-
 	// Writing graph as dot file
-	ioutil.WriteFile("simple_graph.dot", []byte(g.ToDot()), 0644)
+	ioutil.WriteFile("basic_symbolicdiff.dot", []byte(g.ToDot()), 0644)
+
 }
